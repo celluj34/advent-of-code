02cd0ca9fc8392ddd92745938a61e11107ad0764
Joe Cellucci <celluj34@gmail.com>
2021-12-06 22:17:05 -0600

    get 01/b working

---
+++ 01/b/index.ts
@@ -0,0 +1,42 @@
+import input from "./input";
+
+const start = async () => {
+  const lines = input.split("\n").map((x) => parseInt(x));
+
+  let values: Array<number> = [];
+  let count = 0;
+
+  for (const value of lines) {
+    if (values.length !== 3) {
+      values.push(value);
+
+      continue;
+    }
+
+    const currentValue = values.reduce(
+      (previous, current) => previous + current,
+      0
+    );
+
+    values.shift();
+    values.push(value);
+
+    const newValue = values.reduce(
+      (previous, current) => previous + current,
+      0
+    );
+
+    console.log(currentValue);
+    console.log(newValue);
+
+    if (newValue > currentValue) {
+      ++count;
+    }
+  }
+
+  console.log(`number of increases: ${count}`);
+};
+
+(async () => {
+  await start();
+})();
