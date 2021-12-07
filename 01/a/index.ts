7e2377ba4695d8ac91540d8187816b790dcf7436
Joe Cellucci <celluj34@gmail.com>
2021-12-06 22:10:04 -0600

    change input to ts module

--- 01/a/index.ts
+++ 01/a/index.ts
@@ -1,19 +1,7 @@
-import { readFile } from "fs";
-import { resolve, dirname } from "path";
-
-const inputFilePath = resolve(dirname("./input.txt"), "input.txt");
+import input from "./input";
 
 const start = async () => {
-  const lines = await new Promise<Array<number>>((resolve, reject) => {
-    readFile(inputFilePath, (x, y) => {
-      const result = y
-        .toString()
-        .split("\r\n")
-        .map((b) => parseInt(b, 0));
-
-      resolve(result);
-    });
-  });
+  const lines = input.split("\n").map((x) => parseInt(x));
 
   let lastValue = lines[0];
   let count = 0;
