2dd1f89873516c50792e741615a5295568b125cc
Joe Cellucci <celluj34@gmail.com>
2022-12-07 00:19:49 -0600

    complete day 2

---
+++ 2022/AoC.Console/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace AoC.Console
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum value)
+        {
+            var name = value.ToString();
+            var memberInfo = value.GetType().GetMember(name).FirstOrDefault();
+
+            return memberInfo == null ? name : memberInfo.GetDisplayName();
+        }
+
+        public static T? GetDefaultValue<T>(this Enum value)
+        {
+            var name = value.ToString();
+            var memberInfo = value.GetType().GetMember(name).FirstOrDefault();
+
+            return memberInfo == null ? default : memberInfo.GetDefaultValue<T>();
+        }
+    }
+}
