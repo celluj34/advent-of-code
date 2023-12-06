49fa6bc1d242788dfb18619988dbf23834f09199
Joe Cellucci <celluj34@gmail.com>
2023-12-05 22:08:59 -0600

    add initial solution

---
+++ 2023/AoC.Console/Extensions/EnumExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace AoC.Console.Extensions;
+
+public static class EnumExtensions
+{
+    public static string GetDisplayName(this Enum value)
+    {
+        var name = value.ToString();
+        var memberInfo = value.GetType().GetMember(name).FirstOrDefault();
+
+        return memberInfo == null ? name : memberInfo.GetDisplayName();
+    }
+
+    public static T? GetDefaultValue<T>(this Enum value)
+    {
+        var name = value.ToString();
+        var memberInfo = value.GetType().GetMember(name).FirstOrDefault();
+
+        return memberInfo == null ? default : memberInfo.GetDefaultValue<T>();
+    }
+}
---
+++ 2023/AoC.Console/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC.Console.Extensions;
+
+public static class EnumerableExtensions
+{
+    public static ulong Sum<TSource>(this IEnumerable<TSource> source, Func<TSource, ulong> summer)
+    {
+        return source.Aggregate(0UL, (current, item) => current + summer(item));
+    }
+
+    public static IEnumerable<T> TakeUntilInclusive<T>(this IEnumerable<T> collection, Predicate<T> endCondition)
+    {
+        foreach (var item in collection)
+        {
+            yield return item;
+
+            if (endCondition(item))
+            {
+                break;
+            }
+        }
+    }
+}
