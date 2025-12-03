7d00a7a3b5199670c97a9090ce759c926d8ffa52
Joseph Cellucci <celluj34@gmail.com>
2025-12-02 20:39:43 -0600

    day 02.b

---
+++ 2025/AdventOfCode/02/B.cs
@@ -0,0 +1,55 @@
+﻿namespace AdventOfCode._02;
+
+public class B
+{
+    public void Execute()
+    {
+        var input = Input.Day2.Split(",")
+                         .Select(x =>
+                         {
+                             var idPairs = x.Split("-");
+
+                             var start = long.Parse(idPairs[0]);
+                             var end = long.Parse(idPairs[1]);
+
+                             return new
+                             {
+                                 Start = start,
+                                 Count = end - start + 1
+                             };
+                         });
+
+        var total = input.Sum(item =>
+        {
+            long sum = 0;
+            for (var value = item.Start; value < item.Start + item.Count; value++)
+            {
+                var valueString = value.ToString();
+
+                if (DigitRepeats(valueString))
+                {
+                    sum += value;
+                }
+            }
+
+            return sum;
+        });
+
+        Console.WriteLine($"The total is: {total}");
+    }
+
+    private bool DigitRepeats(string value)
+    {
+        var maxChunkLength = value.Length / 2;
+
+        for (var chunkLength = 1; chunkLength <= maxChunkLength; chunkLength++)
+        {
+            if (value.Chunk(chunkLength).Select(x => new string(x)).Distinct().Count() == 1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
