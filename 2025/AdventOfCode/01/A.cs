ffcb1a6e25c78524543de634351c946977377d2f
Joseph Cellucci <celluj34@gmail.com>
2025-12-02 18:59:06 -0600

    day 1.a

---
+++ 2025/AdventOfCode/01/A.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode._01;
+
+public class A
+{
+    public void Execute()
+    {
+        var password = 0;
+        var position = 50;
+        var input = Input.Day1.Split(Environment.NewLine).Select(x => int.Parse(x[1..]) * (x[0] == 'L' ? -1 : 1));
+
+        foreach (var value in input)
+        {
+            position += value;
+
+            while (position < 0)
+            {
+                position += 100;
+            }
+
+            while (position >= 100)
+            {
+                position -= 100;
+            }
+
+            if (position == 0)
+            {
+                password++;
+            }
+        }
+
+        Console.WriteLine($"The password is: {password}");
+    }
+}
