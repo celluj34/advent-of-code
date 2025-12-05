a3426506fd369f564d27c9fe0cda10f340d514ef
Joseph Cellucci <celluj34@gmail.com>
2025-12-04 23:21:36 -0600

    day 05.a

---
+++ 2025/AdventOfCode/05/A.cs
@@ -0,0 +1,28 @@
+﻿namespace AdventOfCode._05;
+
+public class A
+{
+    public void Execute()
+    {
+        var lines = Input.Day5.Split(Environment.NewLine).ToList();
+
+        var freshIngredientIds = lines.TakeWhile(x => x != string.Empty)
+                                      .Select(value =>
+                                      {
+                                          var parts = value.Split("-");
+                                          var first = long.Parse(parts[0]);
+                                          var last = long.Parse(parts[1]);
+
+                                          return (min: first, max: last);
+                                      })
+                                      .OrderBy(x => x.min)
+                                      .ThenBy(x => x.max)
+                                      .ToList();
+
+        var allIngredients = lines.Skip(freshIngredientIds.Count + 1).Select(long.Parse).ToList();
+
+        var freshIngredients = allIngredients.Count(x => freshIngredientIds.Any(tuple => tuple.min <= x && x <= tuple.max));
+
+        Console.WriteLine($"The total is: {freshIngredients}");
+    }
+}
