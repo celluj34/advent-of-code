79bf1e766ac97a068947db35c01e862f8c9a5a22
Joseph Cellucci <celluj34@gmail.com>
2025-12-04 23:34:55 -0600

    day 05.b

---
+++ 2025/AdventOfCode/05/B.cs
@@ -0,0 +1,60 @@
+﻿namespace AdventOfCode._05;
+
+public class B
+{
+    public void Execute()
+    {
+        var lines = Input.Day5.Split(Environment.NewLine).ToList();
+
+        var freshIngredientIdRanges = lines.TakeWhile(x => x != string.Empty)
+                                           .Select(value =>
+                                           {
+                                               var parts = value.Split("-");
+                                               var first = long.Parse(parts[0]);
+                                               var last = long.Parse(parts[1]);
+
+                                               return (min: first, max: last);
+                                           })
+                                           .OrderBy(x => x.min)
+                                           .ThenBy(x => x.max)
+                                           .ToList();
+
+        var freshIngredientIds = new List<(long min, long max)>
+        {
+            freshIngredientIdRanges.First()
+        };
+
+        foreach (var (newMin, newMax) in freshIngredientIdRanges.Skip(1))
+        {
+            var (currentMin, currentMax) = freshIngredientIds.Last();
+
+            if (IsAlreadyIncluded(currentMin, newMin, newMax, currentMax))
+            {
+                continue;
+            }
+
+            if (ShouldExtendCurrentRange(newMin, currentMax, newMax))
+            {
+                freshIngredientIds[^1] = (currentMin, newMax);
+            }
+            else
+            {
+                freshIngredientIds.Add((newMin, newMax));
+            }
+        }
+
+        var freshIngredientIdCount = freshIngredientIds.Sum(x => x.max - x.min + 1);
+
+        Console.WriteLine($"The total is: {freshIngredientIdCount}");
+    }
+
+    private static bool IsAlreadyIncluded(long currentMin, long newMin, long newMax, long currentMax)
+    {
+        return currentMin <= newMin && newMax <= currentMax;
+    }
+
+    private static bool ShouldExtendCurrentRange(long newMin, long currentMax, long newMax)
+    {
+        return newMin <= currentMax && currentMax < newMax;
+    }
+}
--- 2025/AdventOfCode/Program.cs
+++ 2025/AdventOfCode/Program.cs
@@ -4,7 +4,7 @@ public static class Program
 {
     public static void Main()
     {
-        var program = new AdventOfCode._04.B();
+        var program = new AdventOfCode._05.B();
 
         program.Execute();
     }
