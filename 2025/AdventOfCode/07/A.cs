90be40f8d218a86899f552efb916fd16c8c8ba6e
Joseph Cellucci <celluj34@gmail.com>
2025-12-07 00:17:26 -0600

    day 07.a

---
+++ 2025/AdventOfCode/07/A.cs
@@ -0,0 +1,45 @@
+﻿namespace AdventOfCode._07;
+
+public class A
+{
+    public void Execute()
+    {
+        long total = 0;
+        var lines = Input.Day7.Split(Environment.NewLine).Select(x => x.ToList()).ToList();
+
+        for (var i = 1; i < lines.Count; i++)
+        {
+            var rowAbove = lines[i - 1];
+            var row = lines[i];
+
+            for (var j = 0; j < row.Count; j++)
+            {
+                if (row[j] is '^')
+                {
+                    row[j - 1] = '|';
+                    row[j + 1] = '|';
+                }
+                else if (rowAbove[j] is 'S' or '|')
+                {
+                    row[j] = '|';
+                }
+            }
+        }
+
+        for (var i = 1; i < lines.Count; i++)
+        {
+            var rowAbove = lines[i - 1];
+            var row = lines[i];
+
+            for (var j = 0; j < row.Count; j++)
+            {
+                if (row[j] is '^' && rowAbove[j] is '|')
+                {
+                    total++;
+                }
+            }
+        }
+
+        Console.WriteLine($"The total is: {total}");
+    }
+}
