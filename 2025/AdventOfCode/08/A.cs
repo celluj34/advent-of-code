cd24a1e5f94d7baeed3de3f19b8c622fc5f79968
Joseph Cellucci <celluj34@gmail.com>
2025-12-08 00:18:12 -0600

    day 08.a

---
+++ 2025/AdventOfCode/08/A.cs
@@ -0,0 +1,123 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode._08;
+
+public class A
+{
+    public void Execute()
+    {
+        var coordinates = Input.Day8.Split(Environment.NewLine)
+                               .Select(line =>
+                               {
+                                   var list = line.Split(",", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+                                                  .Select(int.Parse)
+                                                  .ToList();
+
+                                   var (x, y, z) = (list[0], list[1], list[2]);
+
+                                   return new Vector3(x, y, z);
+                               })
+                               .OrderBy(x => x.X)
+                               .ThenBy(x => x.Y)
+                               .ThenBy(x => x.Z)
+                               .ToList();
+
+        var coordinatesByDistance = coordinates.Join(coordinates, _ => true, _ => true, (from, to) => new Vector3Pair(from, to))
+                                               .DistinctBy(x => x.GetHashCode())
+                                               .Where(x => x.Distance != 0)
+                                               .OrderBy(x => x.Distance)
+                                               .Take(1000)
+                                               .ToList();
+
+        var groupsOfCoordinates = new List<HashSet<Vector3>>();
+
+        foreach (var (from, to) in coordinatesByDistance)
+        {
+            var existingGroups = groupsOfCoordinates.Where(g => g.Contains(from) || g.Contains(to)).ToList();
+
+            if (!existingGroups.Any())
+            {
+                groupsOfCoordinates.Add([
+                    from,
+                    to
+                ]);
+            }
+            else if (existingGroups.Count is 1)
+            {
+                var existingGroup = existingGroups.Single();
+                existingGroup.Add(from);
+                existingGroup.Add(to);
+            }
+            else
+            {
+                var combinedGroup = new HashSet<Vector3>
+                {
+                    from,
+                    to
+                };
+
+                foreach (var existingGroup in existingGroups)
+                {
+                    groupsOfCoordinates.Remove(existingGroup);
+
+                    foreach (var coordinate in existingGroup)
+                    {
+                        combinedGroup.Add(coordinate);
+                    }
+                }
+
+                groupsOfCoordinates.Add(combinedGroup);
+            }
+        }
+
+        var total = groupsOfCoordinates.Select(x => x.Count).OrderByDescending(x => x).Take(3).Product();
+
+        Console.WriteLine($"The total is: {total}");
+    }
+
+    public readonly struct Vector3Pair : IEquatable<Vector3Pair>
+    {
+        private readonly int _hash;
+
+        public Vector3Pair(Vector3 from, Vector3 to)
+        {
+            From = from;
+            To = to;
+            Distance = Vector3.Distance(from, to);
+
+            var orderedPoints = new List<Vector3>
+                {
+                    From,
+                    To
+                }.OrderBy(x => Vector3.Distance(Vector3.Zero, x))
+                 .ToList();
+
+            _hash = HashCode.Combine(orderedPoints.First(), orderedPoints.Last());
+        }
+
+        public Vector3 From { get; }
+        public Vector3 To { get; }
+        public float Distance { get; }
+
+        public override int GetHashCode()
+        {
+            return _hash;
+        }
+
+        public void Deconstruct(out Vector3 from, out Vector3 to)
+        {
+            from = From;
+            to = To;
+        }
+
+        public bool Equals(Vector3Pair other)
+        {
+            return _hash == other._hash && From.Equals(other.From) && To.Equals(other.To) && Distance.Equals(other.Distance);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Vector3Pair other && Equals(other);
+        }
+    }
+}
